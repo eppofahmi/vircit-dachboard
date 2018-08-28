2d4b8e1b934cc0b610e95ccb19bd752be5ef17a4
eppofahmi <eppofahmi@gmail.com.com>
2018-08-28 18:22:26 +0700

    removing duplicate

--- R/data.R
+++ R/data.R
@@ -1,93 +1,47 @@
 library(tidyverse)
 library(textclean)
 library(ggplot2)
+library(stringr)
+library(textclean)
 
 # data ----
 tabelOutput <- read.csv("https://raw.githubusercontent.com/eppofahmi/vircit-dachboard/master/data/tabelOutput.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+# duplicate teks clean ----
+tabelOutput <- tabelOutput %>%
+  mutate(is_dup = duplicated(teks_clean)) %>%
+  filter(is_dup == FALSE)
+tabelOutput$teks_clean <- gsub("\\bselamat pagi\\b", "", tabelOutput$teks_clean)
+# word count per rows ----
+tabelOutput$wordCount <- str_count(tabelOutput$teks_clean, '\\s+')+1
+tabelOutput <- tabelOutput %>% 
+  filter(wordCount >= 2)
 
-tabelOutput$parameter <- gsub("\\bJangan kembali renggut kebebasan masyarakat jawa barat untuk memilih transportasi!\\b", "memilih transportasi",
-                              tabelOutput$parameter)
-tabelOutput$parameter <- gsub("\\bDukung keberadaan gojek di Purwokerto\\b", "Gojek di Purwokerto",
-                              tabelOutput$parameter)
-tabelOutput$parameter <- gsub("\\blayanan transportasi berbasis online\\b", "",
-                              tabelOutput$parameter)
+# cleaning 
+tabelOutput <- tabelOutput %>%
+  select(kasus, sumber_data, parameter, tanggal, teks, teks_clean)
+tabelOutput$parameter <- tolower(tabelOutput$parameter)
+tabelOutput$parameter <- gsub("[^[:alnum:][:space:]@_]", "", tabelOutput$parameter)
 
-# cleaning ----
-clean_text <- tweet_cleaner(tabelOutput$teks)
-tabelOutput$teks_clean <- clean_text$clean_text
+# parameter petisi change
+tabelOutput$parameter <- gsub("\\bsegera cabut\\b", "petisi_change1", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\brevoke\\b", "petisi_change2", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\bpak jokowi\\b", "petisi_change3", tabelOutput$parameter)
 
-# removing balnk rows
-tabelOutput <- tabelOutput[!(is.na(tabelOutput$teks_clean) | tabelOutput$teks_clean==""), ]
-  
-# write csv ----
-write_csv(tabelOutput, path = "//Volumes/mydata/rSkilss/vircit-dachboard/data/tabelOutput.csv")
+# parameter petisi gojek
+tabelOutput$parameter <- gsub("\\bgojek grabbikedll\\b", "petisi_gojek1", tabelOutput$parameter)
 
-# data analysis ----
-tm_petisi_change <- read.csv(paste0(getwd(), "/data/tm_petisi_change.csv"), 
-                             stringsAsFactors = FALSE)
-tm_petisi_change <- tm_petisi_change %>%
-  mutate(sumber_data = "petisi", 
-         kasus = "change")
+tabelOutput$parameter <- gsub("\\bgojek di purwokerto\\b", "petisi_gojek2", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\bhentikan permenhub 108\\b", "petisi_gojek3", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\bkami membutuhkan gojek\\b", "petisi_gojek4", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\bmemilih transportasi\\b", "petisi_gojek5", tabelOutput$parameter)
+tabelOutput$parameter <- gsub("\\btolak pencabutan gojek tasikmalaya\\b", "petisi_gojek6", tabelOutput$parameter)
 
-tm_petisi_gojek <- read.csv(paste0(getwd(), "/data/tm_petisi_gojek.csv"), 
-                             stringsAsFactors = FALSE)
-tm_petisi_gojek <- tm_petisi_gojek %>%
-  mutate(sumber_data = "petisi_gojek", 
-         kasus = "gojek")
-
-tm_playstore_customer <- read.csv(paste0(getwd(), "/data/tm_playstore_customer.csv"), 
-                            stringsAsFactors = FALSE)
-tm_playstore_customer <- tm_playstore_customer %>%
-  mutate(sumber_data = "playsotore_customer", 
-         kasus = "gojek")
-
-tm_playstore_driver <- read.csv(paste0(getwd(), "/data/tm_playstore_driver.csv"), 
-                            stringsAsFactors = FALSE)
-tm_playstore_driver <- tm_playstore_driver %>%
-  mutate(sumber_data = "playsotore_driver", 
-         kasus = "gojek")
-
-tm_twit_change_btr <- read.csv(paste0(getwd(), "/data/tm_twit_change_btr.csv"), 
-                            stringsAsFactors = FALSE)
-tm_twit_change_btr <- tm_twit_change_btr %>%
-  mutate(sumber_data = "twitter", 
-         kasus = "change")
-
-tm_twit_gojek <- read.csv(paste0(getwd(), "/data/tm_twit_gojek.csv"), 
-                            stringsAsFactors = FALSE)
-tm_twit_gojek <- tm_twit_gojek %>%
-  mutate(sumber_data = "twitter", 
-         kasus = "gojek")
-
-analisis_data <- bind_rows(tm_petisi_change, tm_petisi_gojek, tm_playstore_customer, 
-                           tm_playstore_driver, tm_twit_change_btr, tm_twit_gojek)
-
-class(analisis_data)
-
-write_csv(analisis_data, path = "//Volumes/mydata/rSkilss/vircit-dachboard/data/analisis_data.csv")
-
-# tes visualisasi ----
-analisis_data %>%
-  filter(kasus == "change") %>%                # input 1 - kasus
-  filter(sumber_data == "petisi") %>%          # input 2 - sumber data
-  select(topic, term, beta) %>%
-  filter(topic == 5) %>%                       # input 3 - nomor topik
-  group_by(topic) %>%
-  top_n(5) %>%                                 # input 4 - jumlah term/bigram
-  ggplot(aes(reorder(term, beta), beta, fill = as.factor(topic))) + 
-  geom_col(show.legend = FALSE) + 
-  coord_flip() + facet_wrap(~topic, scales = "free") + 
-  labs(x = NULL)
-
-
-daftarTable <- tabelOutput %>%
-  select(sumber_data, kasus) %>%
-  group_by(sumber_data, kasus) %>%
-  count(sumber_data) %>%
-  select(sumber_data, kasus)
-
-
-sumberData <- tabelOutput %>%
-  select(sumber_data, kasus) %>%
+sumber_data <- tabelOutput %>%
   group_by(kasus) %>%
   count(sumber_data)
+
+parameter <- tabelOutput %>%
+  group_by(kasus, sumber_data) %>%
+  count(parameter)
+
+write_csv(tabelOutput, path = "/Volumes/mydata/rSkilss/vircit-dachboard/data/tabelOutput.csv")
